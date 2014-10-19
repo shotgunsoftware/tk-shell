d036a99ce48448cfeae569ae0dee0d2a58c033f5
Manne Öhrström <manne@shotgunsoftware.com>
2014-10-19 19:45:40 +0100

    Fixed a bug causing modal dialogs not to return a status code. Previously modal dialogs running in this engine have returned None rather than a QtGui.QDialog.DialogCode. This small change ensures that the modal dialog return value propages properly to the caller.

--- engine.py
+++ engine.py
@@ -200,7 +200,7 @@ class ShellEngine(Engine):
                         # the trick of activating + raising does not seem to be enough for
                         # modal dialogs. So force put them on top as well.
                         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | self.windowFlags())
-                        QtGui.QDialog.exec_(self)
+                        return QtGui.QDialog.exec_(self)
                         
                 
                 base["qt_core"] = QtCore
